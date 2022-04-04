bf71b49cdd772060ce592c216da16bb69b2fea55
ECOMT\npena <npena@ecomt.net>
2022-04-04 13:29:38 +0200

    creando las redux actions

--- src/container/redux/actions/productActions.js
+++ src/container/redux/actions/productActions.js
@@ -1,3 +1,17 @@
+import { ActionTypes } from "../constants/action-types"
+
 export const setProducts = (products) =>{
-    
+    //esta constante lo que va a hacer será listarnos los productos gracias a un array
+    return {
+        type: ActionTypes.SET_PRODUCTS, //el tipo de accion que querremos mostrar
+        payload: products,
+    }
+}
+
+export const selectedProducts = (products) =>{
+    //esta constante mostrara los productos que son seleccionados y los muestra
+    return{
+        type: ActionTypes.SELECTED_PRODUCT,
+        payload: products,
+    }
 }
