35103f84eb1223e91cb3b5c76df9c1bd5e4cd624
ECOMT\npena <npena@ecomt.net>
2022-04-04 17:37:23 +0200

    añadiendo enrutamiento al proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css"
 import Header from './container/containers/Header';
+//añadiendo enrutamiento a nuestro proyecto
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductListing from "./container/containers/ProductListing";
+import ProductDetail from "./container/containers/ProductDetail";
+
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Router>
+        <Header/> 
+          <Routes>
+            {/* creacion de las primeras rutas */}
+            <Route path="/"  element={<ProductListing/>}/>
+            <Route path="/product/:productId"   element={<ProductDetail/>}/>
+            <Route>404 Not Found!</Route>
+          </Routes>
+      </Router>
     </div> 
   );
 }
