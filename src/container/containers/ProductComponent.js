822f361abd29778ee3b4dd7ff221a3f12f98fb0b
ECOMT\npena <npena@ecomt.net>
2022-04-05 10:47:29 +0200

    mostrado productos en cards

--- src/container/containers/ProductComponent.js
+++ src/container/containers/ProductComponent.js
@@ -5,24 +5,35 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () =>{
     const products= useSelector((state) => state.allProducts.products); //dentro de todos los productos esta nuestro producto, el cual será el que necesitamos
-    //desestructuramos producto para asi poder obtener cada una de las propiedades
-   // const {id,title} = products[0]; //esto vendra  del producto, me va a dar una matriz de productos 
-    //en este caso la matriz le ponemos productos cero porque solo tenemos un registro
-    return(
-        <div className="four column wide">
+
+    //mostraremos los datos completos del producto en nuestro componente
+    //crearemos una constante que renderizará una vista con los productos 
+    const renderList = products.map((product) => {  //mapea la lista con los product
+        //la consante que nos determinara los datos que vamos a mostrar
+        const {id,title,image,price,category} = product; //todo esto viene del objeto product
+            return( //añadiremos el id identificativo de cada uno de los cards
+                <div className="four wide column" key={id}>
             <div className="ui link cards">
                 {/*tarjetas de enlace ^ */}
                 <div className="card">
                     {/* la tarjeta como tal ^ */}
-                    <div className="image"> </div>
+                    <div className="image"> 
+                        <img src={image} alt={title}/>
+                    </div>
                     <div className="content">
-                        {/* <div className="header">{title}</div> */}
+                        <div className="header">{title}</div>
+                        <div className="meta price"> $ {price}</div>.
+                        <div className="meta">  {category}</div>
                     </div>
                 </div>
 
             </div>
         </div>
-    );
-}
+        );
+    })
+
+    return <>{renderList}</>;  //veremos la lista de renderizado
+    
+};
 
 export default ProductComponent;
