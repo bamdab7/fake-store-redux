10d32a19ba82658478faa8241854e75cdde8756e
ECOMT\npena <npena@ecomt.net>
2022-04-05 09:42:30 +0200

    llamada de axios a la api

--- src/container/containers/ProductListing.js
+++ src/container/containers/ProductListing.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
+//importamos los axios para hacer consultas a nuestra API
+import axios from "axios";
 import { useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
 
@@ -7,6 +9,22 @@ import ProductComponent from "./ProductComponent";
 const ProductListing = () =>{ //busqueda del producto de la tienda
     //usamos un selector de uso para poder obtener los productos
     const products= useSelector((state) => state); //nos devolvera un estado (productos)
+
+//funcion que obtendrá todos los productos
+    const fetchProducts = async ()=>{ //funcion asincrona ya que viene por http [FUNCION BUSQUEDA PRODUCTOS]    
+        //constante que da una respuesta
+        const response = await axios.get("https://fakestoreapi.com/products")
+            .catch((err)=>{
+                console.log("error", err);
+            }); //para manejar el error 
+            //registro de la respuesta por consola, y asi se ve lo que se obtiene
+        console.log(response.data); //datos de respuesta del producto
+    }
+    //funcion de busqueda, la cual llama a buscar productos
+    useEffect(() => {
+        fetchProducts();
+    });
+
     console.log(products); //lo muestra dentro de un array
     return(
         <div className="ui grid container">
